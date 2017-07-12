08f0506196ccb3cbdc6c406f5c8829360f9f6238
danihbelan <danihb_94@hotmail.com>
2017-07-12 11:25:22 +0200

    Cambios en codigos de erro

--- public/dist/allmin.js
+++ public/dist/allmin.js
@@ -151,27 +151,30 @@ angular.module('miApp', [
  * Created by danihbelan on 9/6/17.
  * Controlador del estado welcome
  */
-(function(){
+(function () {
     function welcomeCtrl($http, $state) {
         var vm = this;
-        vm.puntuacion = []
+        //vm.puntuacion = []
+        vm.puntos = [1, 2, 3, 4, 5]
+
 
         $http.get('/users/listaEmpresas').then(
             function (responseOk) {
+                vm.feedback = ''
                 vm.listaEmpresas = responseOk.data
                 console.log(vm.listaEmpresas)
             }, function (responseFail) {
-                vm.empresas=['No hay empresas registradas']
+                vm.feedback = 'No hay empresas registradas'
             }
         )
 
-        vm.puntuaEmpresa=function (id) {
-            console.log('actualizando empresa ', id, ' con puntuacion ',vm.puntuacion[id])
-            var datosPuntuacion={id: id, puntuacion: vm.puntuacion[id]}
+
+        vm.puntuaEmpresa = function (id) {
+            console.log('actualizando empresa ', id, ' con puntuacion ', vm.puntuacion[id])
+            var datosPuntuacion = {id: id, puntuacion: vm.puntuacion[id]}
             $http.post('/users/puntuaEmpresa', datosPuntuacion).then(
                 function (responseOk) {
                     console.log('Peticion correcta')
-                    $state.go('auth.welcome')
                 }, function (responseFail) {
                     console.log('Peticion incorrecta')
                 }
